776af4144f3e10fffda3f94c2f1645b8c1263b91
Chris Martin <ch.martin@gmail.com>
2019-03-02 14:23:28 -0700

    indented code block parsing

--- mdtar/library/Text/MarkdownTAR.hs
+++ mdtar/library/Text/MarkdownTAR.hs
@@ -4,15 +4,32 @@
 
 module Text.MarkdownTAR where
 
-import qualified Data.Char as C
-import Control.Applicative ((<|>))
+import Control.Applicative ((<|>), many, liftA2)
 import Control.Monad (mfilter)
+import qualified Data.Char as C
+import Data.Foldable (fold)
+import Data.Functor (($>))
+import Data.Semigroup (stimes)
 
-import Data.Attoparsec.Text as P
-import Data.Attoparsec.Text (Parser)
+import qualified Data.Attoparsec.Text as P
+import Data.Attoparsec.Text (Parser, endOfLine)
 
 import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import qualified Data.Text.Lazy.Builder as TB
 import Data.Text (Text)
+
+buildText :: TB.Builder -> Text
+buildText = LT.toStrict . TB.toLazyText
+
+(<+>) :: Semigroup a => Parser a -> Parser a -> Parser a
+(<+>) = liftA2 (<>)
+
+foldMany :: Monoid a => Parser a -> Parser a
+foldMany p = fold <$> many p
+
+match :: Parser a -> Parser Text
+match p = fst <$> P.match p
 
 pCode :: Parser Text
 pCode =
@@ -27,20 +44,34 @@ pCodeFenced =
   where
     fence = "```"
 
-    open = P.string fence *> P.skipWhile (not . P.isEndOfLine) *> P.endOfLine
-    close = P.string fence *> P.skipMany pHorizontalSpace *> P.endOfLine
+    open = P.string fence *> P.skipWhile (not . P.isEndOfLine) *> endOfLine
+    close = P.string fence *> P.skipMany pHorizontalSpace *> endOfLine
 
-    code = fst <$> P.match (P.skipMany lineOfCode)
+    code = match (P.skipMany lineOfCode)
 
     lineOfCode =
         mfilter (not . (fence `T.isPrefixOf`))
                 (P.takeWhile (not . P.isEndOfLine))
-        <* P.endOfLine
+        <* endOfLine
 
 pCodeIndented :: Parser Text
 pCodeIndented =
 
-    _
+  do
+    i <- pIndentation
+    firstLine <- takeLine
+    subsequentLines <- foldMany $
+        foldMany emptyLine <+> (P.string i *> takeLine)
+    return (buildText (firstLine <> subsequentLines))
+
+  where
+    emptyLine = many pHorizontalSpace *> endOfLine $> (mempty :: TB.Builder)
+    takeLine = TB.fromText <$> match (P.skipWhile (not . P.isEndOfLine) *> endOfLine)
+
+pIndentation :: Parser Text
+pIndentation =
+
+    P.string (stimes 4 " ") <|> P.string "\t"
 
 pPath :: Parser Text
 pPath =
@@ -50,21 +81,19 @@ pPath =
 pPathHash :: Parser Text
 pPathHash =
 
-    hashes *> path <* eol
+    hashes *> path <* endOfLine
 
   where
-    hashes = P.string "##" *> mfilter (/= '#') peekChar'
+    hashes = P.string "##" *> mfilter (/= '#') P.peekChar'
     path = T.strip <$> P.takeWhile1 (not . P.isEndOfLine)
-    eol = P.endOfLine
 
 pPathUnderline :: Parser Text
 pPathUnderline =
 
-    text <* eol <* underline <* trailingWhitespace <* eol
+    text <* endOfLine <* underline <* trailingWhitespace <* endOfLine
 
   where
     text = T.strip <$> P.takeWhile1 (not . P.isEndOfLine)
-    eol = P.endOfLine
     underline = P.skipMany1 (P.char '-')
     trailingWhitespace = P.skipMany pHorizontalSpace
 
